5a716d816fb8b52375f35edf69ad225f7cfb15bd
Aref Afzali <afzaliaref.aa@gmail.com>
2026-04-19 23:01:04 -0400

    Scaffold Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://arefafzali.github.io',
+  integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  vite: {
+    resolve: {
+      alias: { '@': '/src' }
+    }
+  }
+});
